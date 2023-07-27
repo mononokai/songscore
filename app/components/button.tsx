00d23987d6bb01a048686896d807dd99d89aee4b
mononokai <leachda12@icloud.com>
2023-07-27 13:08:13 -0400

    feat: add css to button

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,7 +1,8 @@
 export function CustomButton() {
     return (
         <div className="h-100 w-200">
-            <button className="rounded">Push me</button>
+            <button className="rounded border-white bg-grey border-2 p-5 hover:bg-slate-800 hover:shadow-lg active:bg-slate-700 text-3xl transition duration-200 ease-in-out
+                ">Push me</button>
         </div>
     )
 }

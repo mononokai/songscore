1cf4861bdf502564555f35d7783f755df8d5a832
mononokai <leachda12@icloud.com>
2023-08-11 20:06:25 -0400

    refactor: made CustomButton take in props

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,8 +1,12 @@
-export function CustomButton() {
+interface CustomButtonProps {
+    innerText: string
+}
+
+export function CustomButton({ innerText }: CustomButtonProps) {
     return (
         <div className="h-100 w-200">
             <button className="rounded border-white bg-grey border-2 p-5 hover:bg-slate-800 hover:shadow-lg active:bg-slate-700 text-3xl transition duration-200 ease-in-out
-                ">Push me</button>
+                ">{innerText}</button>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import { CustomButton } from './components/button'
+import { CustomButton } from './components/Button'
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <CustomButton/>
+            <CustomButton innerText="Push me"/>
         </main>
     );
 }
